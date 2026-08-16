19c19b25334ba36837a69a522c47624abe2ba4b3
hyewon <hewon8883@naver.com>
2026-08-16 15:19:06 +0900

    init: wedule backend project setup

---
+++ src/main/java/com/wedule/wedule/WeduleApplication.java
@@ -0,0 +1,13 @@
+package com.wedule.wedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wedule/wedule/WeduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedule.wedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
